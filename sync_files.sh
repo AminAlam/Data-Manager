fddb8604c7243b8e94e0aec1a1446870e25600b5
Amin Alam <ma.alamalhoda@gmail.com>
2023-01-23 13:54:40 +0100

    bash scripts for backup and file syncing

---
+++ sync_files.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+cp -r -f /home/amin/Documents/Data-Manager/src/web/static /home/aaron/Documents/Data-Manager/src/web
+cp -r -f /home/amin/Documents/Data-Manager/src/web/templates /home/aaron/Documents/Data-Manager/src/web
+cp -f /home/amin/Documents/Data-Manager/src/web/api.py /home/aaron/Documents/Data-Manager/src/web
+cp -r -f /home/amin/Documents/Data-Manager/src/utils /home/aaron/Documents/Data-Manager/src/
+cp -f /home/amin/Documents/Data-Manager/src/database/{configs.py,dictianory.py,operators.py} /home/aaron/Documents/Data-Manager/src/database/
+cp -f /home/amin/Documents/Data-Manager/requirements.txt /home/aaron/Documents/Data-Manager/
