25cffddf682bdd488b3b643ca80140d318849d0a
Amin Alam <ma.alamalhoda@gmail.com>
2023-01-23 11:42:24 +0100

    recaptcha activated

--- src/web/api.py
+++ src/web/api.py
@@ -83,7 +83,7 @@ class WebApp():
                 if len(users)==0:
                     return flask.render_template('login.html', error='Invalid username or password', form=form)
                 #elif form.validate_on_submit():
-                elif 1:
+                elif form.validate_on_submit():
                     flask.session['username'] = username
                     flask.session['password'] = password
                     flask.session['logged_in'] = True
