af0cf0d9fc1590a47b59b068461c75e432b5de94
Amin Alam <ma.alamalhoda@gmail.com>
2023-02-28 16:23:56 +0100

    web based file editor is added

---
+++ src/web/flaskcode/__pkginfo__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""package info"""
+
+title = 'flaskcode'
+
+version_info = (0, 0, 8)
+version = '.'.join(map(str, version_info))
+
+license = 'MIT'
+copyright = 'Copyright (c) 2019 Sujeet Kumar <sujeetkv90@gmail.com>'
+
+author = 'Sujeet Kumar'
+email = 'sujeetkv90@gmail.com'
+uri = 'https://github.com/sujeetkv/flaskcode'
+
+description = 'Web based code editor on python flask framework'
+
+status = 'Development'
---
+++ src/web/flaskcode/views.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import os
+import mimetypes
+from flask import render_template, abort, jsonify, send_file, g, request
+from .utils import write_file, dir_tree, get_file_extension
+from . import blueprint
+
+
+@blueprint.route('/')
+def index():
+    dirname = os.path.basename(g.flaskcode_resource_basepath)
+    dtree = dir_tree(g.flaskcode_resource_basepath, g.flaskcode_resource_basepath + '/')
+    return render_template('flaskcode/index.html', dirname=dirname, dtree=dtree)
+
+
+@blueprint.route('/resource-data/<path:file_path>.txt', methods=['GET', 'HEAD'])
+def resource_data(file_path):
+    file_path = os.path.join(g.flaskcode_resource_basepath, file_path)
+    if not (os.path.exists(file_path) and os.path.isfile(file_path)):
+        abort(404)
+    response = send_file(file_path, mimetype='text/plain')
+    mimetype, encoding = mimetypes.guess_type(file_path, False)
+    if mimetype:
+        response.headers.set('X-File-Mimetype', mimetype)
+        extension = mimetypes.guess_extension(mimetype, False) or get_file_extension(file_path)
+        if extension:
+            response.headers.set('X-File-Extension', extension.lower().lstrip('.'))
+    if encoding:
+        response.headers.set('X-File-Encoding', encoding)
+    return response
+
+
+@blueprint.route('/update-resource-data/<path:file_path>', methods=['POST'])
+def update_resource_data(file_path):
+    file_path = os.path.join(g.flaskcode_resource_basepath, file_path)
+    is_new_resource = bool(int(request.form.get('is_new_resource', 0)))
+    if not is_new_resource and not (os.path.exists(file_path) and os.path.isfile(file_path)):
+        abort(404)
+    success = True
+    message = 'File saved successfully'
+    resource_data = request.form.get('resource_data', None)
+    if resource_data:
+        success, message = write_file(resource_data, file_path)
+    else:
+        success = False
+        message = 'File data not uploaded'
+    return jsonify({'success': success, 'message': message})
